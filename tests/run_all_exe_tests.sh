fbb21f2cb8c6e6f836d266179203338699ff79ac
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-11-18 08:31:10 +0100

    automatically creation of virtual environment while testing

--- tests/run_all_exe_tests.sh
+++ tests/run_all_exe_tests.sh
@@ -1,5 +1,20 @@
+set -e
 
+#prepare test enviroment:
 
+ENV_DIR="../p3"
+
+if [ ! -d "$ENV_DIR" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  virtualenv -p python3 "$ENV_DIR"
+  . "$ENV_DIR/bin/activate"
+  pip install https://github.com/realead/exetest/zipball/master
+else
+  . "$ENV_DIR/bin/activate"
+fi
+
+pip freeze
+
+#run tests
 export  PATH="$PATH:../bin"
-
-python2.7 -m unittest discover -s . -v
+python -m unittest discover -s . -v
